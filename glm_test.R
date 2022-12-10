72d98e52ce8d508b879337332bd845a0ca7d9403
Matt Stuart <mstuart1@luc.edu>
2022-12-10 16:06:32 -0600

    Add files via upload

--- glm_test.R
+++ glm_test.R
@@ -13,3 +13,51 @@ summary(mod)
 
 # family = "binomial" means we assume the response is binary 
 # (or follows a binomial distribution)
+
+mod <- glm(Survived ~ Age + Sex,
+           data=Titanic,
+           family="binomial")
+
+# Test Statistic
+# Null Deviance - Residual Deviance = 964.52 - 749.96 = 214.56
+summary(mod)
+
+# p-value
+1 - pchisq(214.56,df=713-711)
+# 0
+
+mod_red <- glm(Survived ~ Age + Sex,
+           data=Titanic,
+           family="binomial")
+
+mod_full <- glm(Survived ~ Age + Sex + as.character(Pclass),
+                data=Titanic,
+                family="binomial")
+
+# Test Statistic
+# Difference in Residual deviance from reduced and full model
+anova(mod_red,mod_full)
+# 102.67
+
+# p-value
+# degrees of freedom from anova
+1 - pchisq(102.67,df=2)
+# 0
+
+# Choose Maximum Model
+mod_max <- glm(Survived ~ Age + Sex + as.character(Pclass) +
+                 SibSp + Parch + Fare + Embarked,
+               data=Titanic,
+               family="binomial")
+
+# Model Selection performed the same as linear regression
+best_mod <- step(mod_max)
+
+# Log-likelihood of best model
+# 636.56/-2
+
+
+
+
+
+
